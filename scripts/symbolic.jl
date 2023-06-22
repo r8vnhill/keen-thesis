a4940f31d8b9341593c2695b8016c23a7c2865f5
R8V <ignacio.slater@ug.uchile.cl>
2023-06-21 20:58:26 -0400

    CH(bg) GP Selection v0.4#2306.10

--- scripts/symbolic.jl
+++ scripts/symbolic.jl
@@ -1,25 +1,125 @@
 using Plots; pythonplot()
 using LaTeXStrings
+using Statistics
 
+"""
+Calculates the mean squared error between the observed and predicted values.
+"""
+function mse(observed::Vector{Float64}, prediction::Vector{Float64})
+  return sum((observed - prediction).^2) / length(observed)
+end
+
+"""
+Sample points.
+"""
+xs = [
+  -0.889160069272859
+  -0.8561029711395651
+  -0.8212951850355155
+  -0.8181934125983823
+  -0.4298586689110253
+  -0.3523275114715019
+  -0.0357759083395397
+  0.017449673577553337
+  0.5290096774879465
+  0.8211010511234629
+]
+
+"""
+Target function: 5x^3 - 2x^2 + sin(x) - 7
+"""
 f(x) = 5x^3 - 2x^2 + sin(x) - 7
 
-# 10 random points in the interval x = [-1, 1]
-xs = sort(rand(10) * 2 .- 1)
-ys = f.(xs)
+"""
+Individual 1: 3 / sin(2) * 5^3
+"""
+I_1(_) = 3 / sin(2) * 5^3
 
-# Plot the function
-plot(f, -1, 1, label="f(x)", lw=2)
-plot!(xs, ys, seriestype=:scatter, label="data")
-xlabel!(L"x")
-ylabel!(L"f(x)")
-title!(L"f(x) = 5x^3 - 2x^2 + \sin(x) - 7")
+"""
+Individual 2: 7 - (5 + sin(x))
+"""
+I_2(x) = 7 - (5 + sin(x))
 
+"""
+Individual 3: 7 + 2
+"""
+I_3(_) = 7.0 + 2
 
-png("img/theoretical_framework/symbolic.png")
+"""
+Individual 4: 5 * x^2
+"""
+I_4(x) = 5 * x.^2
 
-# Save the points to a file
-open("data.txt", "w") do io
-    for (x, y) in zip(xs, ys)
-        println(io, "f($x) = $y")
-    end
+println("Plotting the population and the target function...")
+plot(f, label=L"$f(x)$", lw=2)
+plot!(I_1, label=L"$I_1(x)$", lw=1)
+plot!(I_2, label=L"$I_2(x)$", lw=2, ls=:dot)
+plot!(I_3, label=L"$I_3(x)$", lw=2, ls=:dashdot)
+plot!(I_4, label=L"$I_4(x)$", lw=2, ls=:dash)
+title!("Individuals of the population and the target function")
+xlabel!(L"$x$")
+ylabel!(L"$f(x)$")
+
+png("img/theoretical_framework/gp_pop_init.png")  # Save the plot to a file.
+
+"""
+Mean squared errors.
+"""
+mses = [
+  mse(f.(xs), I_1.(xs))
+  mse(f.(xs), I_2.(xs))
+  mse(f.(xs), I_3.(xs))
+  mse(f.(xs), I_4.(xs))
+]
+println("Mean squared errors: ", mses)
+
+# Save the mean squared errors to a file.
+open("data/bg_gp_sym_mse_init.txt", "w") do io
+  for (i, mse) in zip(eachindex(mses), mses)
+    println(io, "mse(f(x), I_", i, "(x)) = ", mse)
+  end
+  println(io, "average = ", mean(mses))
+  println(io, "std = ", std(mses))
 end
+
+"""
+Corrected fitness values.
+This prioritizes the individuals with the lowest mean squared error.
+"""
+corrected_fitness = [
+  sum(mses) - mses[1]
+  sum(mses) - mses[2]
+  sum(mses) - mses[3]
+  sum(mses) - mses[4]
+]
+
+"""
+Sum of the corrected fitness values.
+"""
+corrected_fitness_sum = sum(corrected_fitness)
+
+"""
+Selection probabilities.
+"""
+probabilities = corrected_fitness / corrected_fitness_sum
+println("Selection probabilities: ", probabilities)
+
+# Save the probabilities to a file.
+open("data/gp_sym_probabilities_1.txt", "w") do io
+  println(io, "Individual  & Fitness & Selection Probability \\\\")
+  println(io, "\\hline")
+  for (i, fitness, prob) in zip(eachindex(mses), mses, probabilities)
+    println(io, "\\(I_", i, "\\) & ", fitness, " & ", prob * 100, "\\% \\\\")
+  end
+  println(io, "\\hline")
+end
+
+println("Plotting the population and the target function...")
+plot(f, -1, 1, label=L"$f(x)$", lw=2)
+plot!(I_2, label=L"$I_2(x)$", lw=2, ls=:dot)
+plot!(I_3, label=L"$I_3(x)$", lw=2, ls=:dashdot)
+title!("Survivors of the population and the target function")
+xlabel!(L"$x$")
+ylabel!(L"$f(x)$")
+
+png("img/theoretical_framework/gp_pop_sel_survivors.png")  # Save the plot to a file.
